6bf67e53a5ddf85f4ba8d15ea01efca3347f27ef
Benjamin Phang Qi Lun <142822176+benneb03@users.noreply.github.com>
2026-06-20 00:15:07 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                res += i.lower()
+
+        return res == res[::-1]
